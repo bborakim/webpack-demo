f97d3d3b6d373e21a1ecb8fd8ab75f6faf714d9a
김보라 <bora.kim@odinue.net>
2019-02-23 15:14:40 +0900

    commit

---
+++ src/js/cube.js
@@ -0,0 +1,10 @@
+/*
+//default export 는 파일이 단 한번만 사용가능
+export default function cube(x) {
+  return x * x * x;
+}*/
+
+// default exports: 화일에 단 한개
+export default function cube(x) {
+  return x * x * x;
+}
